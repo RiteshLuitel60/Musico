df635be4fdf779d33e74dcd11dd19b77dcb5971b
musico <luitelritesh60@gmail.com>
2024-08-12 21:47:43 +1000

    updated how we access the song audio file from out api

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -16,15 +16,6 @@ const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
     dispatch(playPause(true));
   };
 
-/*  const {
-    id,
-    attributes: {
-      name: title,
-      artistName: subtitle,
-      artwork: { url: coverArt } = {},
-      previews = []
-    } = {}
-  } = song;*/
 
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
