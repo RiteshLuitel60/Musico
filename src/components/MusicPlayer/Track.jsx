7670844100a993b988a0aa007668c9c8f38d7380
musico <luitelritesh60@gmail.com>
2024-08-12 20:46:18 +1000

    updated track file in musicplayer to access data from api properly

--- src/components/MusicPlayer/Track.jsx
+++ src/components/MusicPlayer/Track.jsx
@@ -1,30 +1,20 @@
 import React from 'react';
 
-const Track = ({ isPlaying, isActive, activeSong }) => {
-  // Destructure the active song details
-  const {
-    attributes: {
-      name: title,
-      artistName: subtitle,
-      artwork: { url: coverArt } = {}
-    } = {}
-  } = activeSong || {};
+const Track = ({ isPlaying, isActive, activeSong }) => (
+  <div className="flex-1 flex items-center justify-start">
+    <div className={`${isPlaying && isActive ? 'animate-[spin_3s_linear_infinite]' : ''} hidden sm:block h-16 w-16 mr-4`}>
+      <img src={activeSong?.attributes?.artwork?.url} alt="cover art" className="rounded-full" />
 
-  return (
-    <div className="flex-1 flex items-center justify-start">
-      <div className={`${isPlaying && isActive ? 'animate-[spin_3s_linear_infinite]' : ''} hidden sm:block h-16 w-16 mr-4`}>
-        <img src={coverArt || '/default-image.jpg'} alt="cover art" className="rounded-full" />
-      </div>
-      <div className="w-[50%]">
-        <p className="truncate text-white font-bold text-lg">
-          {title || 'No active Song'}
-        </p>
-        <p className="truncate text-gray-300">
-          {subtitle || 'No active Song'}
-        </p>
-      </div>
     </div>
-  );
-};
+    <div className="w-[50%]">
+      <p className="truncate text-white font-bold text-lg">
+        {activeSong?.attributes.artistName ? activeSong?.attributes.artistName : 'No active Song'}
+      </p>
+      <p className="truncate text-gray-300">
+        {activeSong?.attributes?.albumName || 'No album name'}
+      </p>
+    </div>
+  </div>
+);
 
 export default Track;
